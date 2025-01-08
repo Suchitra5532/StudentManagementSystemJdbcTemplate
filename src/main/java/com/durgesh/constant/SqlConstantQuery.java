a46d3352ecc173a976682716a4eb84567aedbe9f
SuchitraKhandale <suchitrakhandale5@gmail.com>
2025-01-08 19:52:40 +0530

    Constant Added.

--- src/main/java/com/durgesh/constant/SqlConstantQuery.java
+++ src/main/java/com/durgesh/constant/SqlConstantQuery.java
@@ -2,7 +2,7 @@ package com.durgesh.constant;
 
 public class SqlConstantQuery {
 
-    public static final String insertRecord = "INSERT INTO Students (first_name, last_name, email, date_of_birth, enrollment_date) VALUES (?, ?, ?, ?, ?)";
-
-    public static final String updateQuery = "UPDATE Students SET first_name = ?, last_name = ?, email = ?, date_of_birth = ?, enrollment_date = ? WHERE student_id = ?";
+    public static final String INSERTRECORD = "INSERT INTO Students (first_name, last_name, email, date_of_birth, enrollment_date) VALUES (?, ?, ?, ?, ?)";
+    public static final String UPDATERECORD = "UPDATE Students SET first_name = ?, last_name = ?, email = ?, date_of_birth = ?, enrollment_date = ? WHERE student_id = ?";
+    public static final String DELETE = "DELETE FROM Students WHERE student_id = ?";
 }
